0816a010f300b9ee7d305a11e651dd556ad10bb9
Ross Cannizzaro <4198648+rcannizzaro@users.noreply.github.com>
2020-07-11 23:02:59 +0930

    Update install.sh

--- build/root/install.sh
+++ build/root/install.sh
@@ -1,22 +1,28 @@
 #!/bin/bash
 
-# exit script if return code != 0
+# Exit script if return code != 0
+
 set -e
 
-# build scripts
-####
+##############################################################################################################################################################################################################
+# Build scripts
+##############################################################################################################################################################################################################
 
-# download build scripts from github
+# Download build scripts from github
+
 curl --connect-timeout 5 --max-time 600 --retry 5 --retry-delay 0 --retry-max-time 60 -o /tmp/scripts-master.zip -L https://github.com/rcannizzaro/devenv-scripts/archive/master.zip
 
-# unzip build scripts
+# Unzip build scripts
+
 unzip -o /tmp/scripts-master.zip -d /tmp
 
-# move shell scripts to /root
+# Move shell scripts to /root
+
 mv /tmp/DevEnv-Scripts-master/shell/arch/docker/*.sh /usr/local/bin/
 
-# detect image arch
-####
+##############################################################################################################################################################################################################
+# Detect image architecture
+##############################################################################################################################################################################################################
 
 OS_ARCH=$(cat /etc/os-release | grep -P -o -m 1 "(?=^ID\=).*" | grep -P -o -m 1 "[a-z]+$")
 if [[ ! -z "${OS_ARCH}" ]]; then
@@ -31,31 +37,32 @@ else
 	OS_ARCH="x86-64"
 fi
 
-# pacman packages
-####
+##############################################################################################################################################################################################################
+# Pacman packages
+##############################################################################################################################################################################################################
 
-# define pacman packages
-pacman_packages="base-devel git tk mc nano mono dotnet-sdk p7zip make gcc gdb dbeaver wine wine-gecko wine-mono chromium w3m"
+# Define pacman packages
 
-# install compiled packages using pacman
+pacman_packages="base-devel git tk mc nano mono dotnet-sdk p7zip make gcc gdb dbeaver wine wine-gecko wine-mono w3m"
+
+# Install compiled packages using pacman
+
 if [[ ! -z "${pacman_packages}" ]]; then
 	pacman -S --needed $pacman_packages --noconfirm
 fi
 
-# aur packages
-####
+##############################################################################################################################################################################################################
+# AUR packages
+##############################################################################################################################################################################################################
 
-# build package only, as apacman seems to be having issues finding the rider built
+# Build package only, as apacman seems to be having issues finding the rider built
 # package, probably related to the fact that the package version has a colon, so
 # this could be a bug in apacman escaping.
 # due to the above issue we build and then use pacman to install manually (done in aur.sh).
+
 aur_build_only="true"
 
-# define aur packages
-# call aur install script (arch user repo)
-
-#aur_packages="gconf-gtk2"
-#source aur.sh
+# Define AUR packages & call aur install script
 
 aur_packages="msbuild-15-bin rider"
 source aur.sh
@@ -76,10 +83,12 @@ source aur.sh
 #aur_build_only="false"
 #source aur.sh
 
-# config novnc
-###
+##############################################################################################################################################################################################################
+# NoVNC Configuration
+##############################################################################################################################################################################################################
 
-# overwrite novnc 16x16 icon with application specific 16x16 icon (used by bookmarks and favorites)
+# Overwrite novnc 16x16 icon with application specific 16x16 icon (used by bookmarks and favorites)
+
 cp /home/nobody/novnc-16x16.png /usr/share/webapps/novnc/app/images/icons/
 
 cat <<'EOF' > /tmp/startcmd_heredoc
@@ -87,10 +96,9 @@ cat <<'EOF' > /tmp/startcmd_heredoc
 # /usr/bin/rider
 EOF
 
-# env vars
-####
+# Environment Variables
 
-# set RIDER_PROPERTIES env var, this determines the path to the custom idea.properties file
+# Set RIDER_PROPERTIES env var, this determines the path to the custom idea.properties file
 # which contains the paths for config, plugins, system and log paths (see config rider section),
 # which are then defined to point at /config/... for persistence.
 
@@ -111,40 +119,49 @@ else
 fi
 EOF
 
-# replace env vars placeholder string with contents of file (here doc)
+# Replace env vars placeholder string with contents of file (here doc)
+
 sed -i '/# ENVVARS_PLACEHOLDER/{
 	s/# ENVVARS_PLACEHOLDER//g
 	r /tmp/envvars_heredoc
 }' /usr/local/bin/init.sh
 rm /tmp/envvars_heredoc
 
-# container perms
-####
+##############################################################################################################################################################################################################
+# Container Permissions
+##############################################################################################################################################################################################################
 
-# define comma separated list of paths 
+# Define comma separated list of paths 
+
 install_paths="/tmp,/usr/share/themes,/home/nobody,/usr/share/webapps/novnc,/usr/share/applications,/usr/share/licenses,/etc/xdg,/usr/share/rider"
 
-# split comma separated string into list for install paths
+# Split comma separated string into list for install paths
+
 IFS=',' read -ra install_paths_list <<< "${install_paths}"
 
-# process install paths in the list
+# Process install paths in the list
+
 for i in "${install_paths_list[@]}"; do
 
-	# confirm path(s) exist, if not then exit
+	# Confirm path(s) exist, if not then exit
+
 	if [[ ! -d "${i}" ]]; then
 		echo "[crit] Path '${i}' does not exist, exiting build process..." ; exit 1
 	fi
 
 done
 
-# convert comma separated string of install paths to space separated, required for chmod/chown processing
+# Convert comma separated string of install paths to space separated, required for chmod/chown processing
+
 install_paths=$(echo "${install_paths}" | tr ',' ' ')
 
-# set permissions for container during build - Do NOT double quote variable for install_paths otherwise this will wrap space separated paths as a single string
+# Set permissions for container during build - Do NOT double quote variable for install_paths otherwise this will wrap space separated paths as a single string
+
 chmod -R 775 ${install_paths}
 
-# create file with contents of here doc, note EOF is NOT quoted to allow us to expand current variable 'install_paths'
+# Create file with contents of here doc, note EOF is NOT quoted to allow us to expand current variable 'install_paths'
 # we use escaping to prevent variable expansion for PUID and PGID, as we want these expanded at runtime of init.sh
+
 cat <<EOF > /tmp/permissions_heredoc
 
 # get previous puid/pgid (if first run then will be empty string)
@@ -166,15 +183,17 @@ echo "\${PGID}" > /root/pgid
 
 EOF
 
-# replace permissions placeholder string with contents of file (here doc)
+# Replace permissions placeholder string with contents of file (here doc)
+
 sed -i '/# PERMISSIONS_PLACEHOLDER/{
     s/# PERMISSIONS_PLACEHOLDER//g
     r /tmp/permissions_heredoc
 }' /usr/local/bin/init.sh
 rm /tmp/permissions_heredoc
 
-# env vars
-####
+##############################################################################################################################################################################################################
+# Environment Variables
+##############################################################################################################################################################################################################
 
-# cleanup
-cleanup.sh
+# Cleanup
+# cleanup.sh
